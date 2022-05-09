6570e5dceefb9d5b6d6ab4bd384a168441938fd6
Drakodan <traxxdata@laposte.net>
2022-05-09 15:16:11 +0200

    mod

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import chalk from 'chalk';
 
 const wilders = ["Sonia ", "Nicolas ", "Loup ", "Daniel "];
-console.log(chalk.blue(["Sonia "]) + chalk.yellow("Nicolas ") + chalk.green("Loup ") + chalk.red("Daniel "));
+console.log(chalk.blue("Sonia ") + chalk.yellow("Nicolas ") + chalk.green("Loup ") + chalk.red("Daniel "));
